db94f53da192dc1bc9c909fa7906f1e57a6ac570
automaton641 <automaton641@gmail.com>
2020-06-01 20:15:15 -0500

    actualizar

--- Sentidos/src/proyecto/sentidos/Fuente.java
+++ Sentidos/src/proyecto/sentidos/Fuente.java
@@ -2,6 +2,7 @@ package proyecto.sentidos;
 
 import java.awt.*;
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.nio.charset.StandardCharsets;
@@ -9,20 +10,13 @@ import java.nio.charset.StandardCharsets;
 public class Fuente {
     Font interna;
     Fuente(String direccion) {
-        InputStream is = ClassLoader.getSystemResourceAsStream("./proyecto");
-        assert is != null;
-        InputStreamReader isr = new InputStreamReader(is, StandardCharsets.UTF_8);
-        BufferedReader br = new BufferedReader(isr);
-        br.lines().forEach(System.out::println);
-        /*
+        InputStream flujoDeDatos = getClass().getClassLoader().getResourceAsStream(direccion);
         GraphicsEnvironment ge = GraphicsEnvironment.getLocalGraphicsEnvironment();
         try {
-            InputStream flujoDeDatos = getClass().getResourceAsStream(direccion);
             ge.registerFont(Font.createFont(Font.TRUETYPE_FONT, flujoDeDatos));
         } catch (FontFormatException | IOException exception) {
             exception.printStackTrace();
             System.exit(1);
         }
-        */
     }
 }
